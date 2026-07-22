dbf6fad3fbee5853fa868fac070c7ae11d8d97aa
Alex Harding <hardingalexh@gmail.com>
2026-07-22 19:03:33 -0400

    removing png files daily

--- data/scraper.py
+++ data/scraper.py
@@ -1,10 +1,20 @@
 import schedule
 import time
+from pathlib import Path
 import leaders
 import stats
 
 
+def remove_root_png_files():
+    for png_file in Path("/app/").glob("*.png"):
+        try:
+            png_file.unlink()
+        except OSError:
+            pass
+
+
 def job():
+    remove_root_png_files()
     leaders.scrape()
     stats.scrape()
 
